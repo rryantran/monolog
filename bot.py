d11572e53074669965b4c169f81747041b029414
ryantran <113406848+rryantran@users.noreply.github.com>
2025-04-12 02:17:03 -0500

    add test message handling

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 import os
 from dotenv import load_dotenv
-from discord import Intents
+from discord import Intents, DMChannel
 from discord.ext import commands
 
 # Load environment variables
@@ -10,6 +10,7 @@ BOT_TOKEN = os.getenv("DISCORD_BOT_TOKEN")
 
 # Set up intents
 intents = Intents.default()
+intents.message_content = True
 intents.dm_messages = True
 
 # Set up commands
@@ -21,6 +22,15 @@ async def on_ready():
     """Print a message when the bot is ready"""
 
     print(f"monolog activated!")
+
+
+@bot.event
+async def on_message(message):
+    """Respond to hello message from user in DMs"""
+
+    if isinstance(message.channel, DMChannel):
+        if message.author != bot.user and message.content.lower() == "hello":
+            await message.channel.send("Hello! this is **monolog**, your journaling companion for Discord.")
 
 if __name__ == "__main__":
     bot.run(BOT_TOKEN)

d6c53ba047069acc47b760a10b15bd406f2ba9af
ryantran <113406848+rryantran@users.noreply.github.com>
2025-05-08 15:59:10 -0500

    switch setup command to slash

--- commands/setup.py
+++ commands/setup.py
@@ -1,4 +1,4 @@
-from discord import Embed
+from discord import Embed, Interaction, app_commands
 from discord.ext import commands
 from db import insert_user, fetch_user
 from visual.colors import dark_purple
@@ -8,30 +8,30 @@ class Setup(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(name="setup")
-    async def setup(self, ctx):
+    @app_commands.command(name="setup")
+    async def setup(self, interaction: Interaction):
         """Set up a user's journal"""
 
-        discord_id = ctx.author.id
+        discord_id = interaction.user.id
 
         if fetch_user(discord_id).data:
             embed = Embed(
                 title="Journal Setup",
-                description="Your journal is already set up.\n\nTo learn more about using your journal, use the command `.help`",
+                description="Your journal is already set up.",
                 color=dark_purple)
 
-            await ctx.send(embed=embed)
+            await interaction.response.send_message(embed=embed)
             return
 
         insert_user(discord_id)
 
         embed = Embed(
             title="Journal Setup",
-            description="Your journal has been set up.\n\nTo learn more about using your journal, use the command `.help`",
+            description="Your journal has been set up.",
             color=dark_purple
         )
 
-        await ctx.send(embed=embed)
+        await interaction.response.send_message(embed=embed)
         return
 
 
